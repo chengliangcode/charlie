c12e6a1aae312898a01ca9cf0787e8249a0fb9f5
charlie <71552682@qq.com>
2022-10-22 15:45:18 +0800

    update

---
+++ charlie-common/src/main/java/com/cl/code/common/util/TreeUtils.java
@@ -0,0 +1,14 @@
+package com.cl.code.common.util;
+
+
+/**
+ * @author chengliang
+ * @since 2022/10/22 15:20
+ */
+public class TreeUtils {
+
+    public static String toTree() {
+        return "111";
+    }
+
+}
