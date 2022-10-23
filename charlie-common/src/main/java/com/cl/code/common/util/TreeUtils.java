839ee1bbbcf7b4b9905a470c4d665327fec13267
charlie <71552682@qq.com>
2022-10-23 11:35:05 +0800

    update

--- charlie-common/src/main/java/com/cl/code/common/util/TreeUtils.java
+++ charlie-common/src/main/java/com/cl/code/common/util/TreeUtils.java
@@ -1,14 +1,22 @@
 package com.cl.code.common.util;
 
 
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * @author chengliang
  * @since 2022/10/22 15:20
  */
+@Data
+@Slf4j
 public class TreeUtils {
 
+    private static String id = "111";
+
     public static String toTree() {
-        return "111";
+        log.debug("值:{}", id);
+        return id;
     }
 
 }
